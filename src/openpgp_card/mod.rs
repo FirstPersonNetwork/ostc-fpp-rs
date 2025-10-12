482f7d4a9ed004b703e0ceec5a5b3f056508594a
Glenn Gore <glenn.g@affinidi.com>
2025-10-12 17:27:57 +0800

    Pulling key info from hardware token
    Signed-off-by: Glenn Gore <glenn.g@affinidi.com>

--- src/openpgp_card/mod.rs
+++ src/openpgp_card/mod.rs
@@ -2,12 +2,74 @@
 *   Handles everything todo with openpgp-card tokens
 */
 
+use crate::{CLI_BLUE, CLI_GREEN, CLI_PURPLE};
 use anyhow::Result;
 use card_backend_pcsc::PcscBackend;
+use chrono::{DateTime, Utc};
 use console::style;
-use openpgp_card::{Card, state::Open};
+use openpgp_card::{
+    Card,
+    ocard::{
+        KeyType,
+        algorithm::AlgorithmAttributes,
+        crypto::PublicKeyMaterial,
+        data::{Features, Fingerprint, KeyGenerationTime, KeySet, KeyStatus, TouchPolicy},
+    },
+    state::{Open, Transaction},
+};
+use std::fmt;
 
-use crate::{CLI_BLUE, CLI_GREEN, CLI_PURPLE};
+#[derive(Default)]
+pub struct KeySlotInfo {
+    /// PGP Public Key Fingerprint
+    fingerprint: Option<String>,
+    /// Time that this key was generated
+    /// 2025-10-02 03:21:06 UTC
+    creation_time: Option<String>,
+    /// Algorithm used for this key
+    algorithm: Option<AlgorithmAttributes>,
+    /// Does this key require touch to use?
+    touch_policy: Option<TouchPolicy>,
+    /// Additional info relating to the touch policy
+    touch_features: Option<Features>,
+    /// Status of the key
+    status: Option<KeyStatus>,
+    /// Public key material
+    public_key_material: Option<Vec<u8>>,
+    /// Number of Digital Signatures created with this key
+    signature_count: Option<u32>,
+}
+
+impl fmt::Debug for KeySlotInfo {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "KeySlotInfo {{")?;
+        if let Some(fp) = &self.fingerprint {
+            writeln!(f, "  Fingerprint: {}", fp)?;
+        }
+        if let Some(ct) = &self.creation_time {
+            writeln!(f, "  Creation Time: {}", ct)?;
+        }
+        if let Some(alg) = &self.algorithm {
+            writeln!(f, "  Algorithm: {}", alg)?;
+        }
+        if let Some(tp) = &self.touch_policy {
+            writeln!(f, "  Touch Policy: {:?}", tp)?;
+        }
+        if let Some(tf) = &self.touch_features {
+            writeln!(f, "  Touch Features: {:?}", tf.to_string())?;
+        }
+        if let Some(status) = &self.status {
+            writeln!(f, "  Status: {:?}", status)?;
+        }
+        if let Some(pk) = &self.public_key_material {
+            writeln!(f, "  Public Key Material: {:02X?}", pk)?;
+        }
+        if let Some(sc) = &self.signature_count {
+            writeln!(f, "  Signature Count: {}", sc)?;
+        }
+        writeln!(f, "}}")
+    }
+}
 
 /// Get a list of active cards on this system
 pub fn cards() -> Result<Vec<Card<Open>>> {
@@ -75,7 +137,70 @@ pub fn print_cards(cards: &mut [Card<Open>]) -> Result<()> {
         } else {
             println!("{}", style("<NOT SET>").color256(CLI_PURPLE).blink());
         }
+
+        // Check key status for this hardware token
+        let fps = open_card.fingerprints()?;
+        let kgt = open_card.key_generation_times()?;
+
+        println!("{}", style("Work in progress...").color256(CLI_PURPLE));
+        let sign_info = get_key_info(&mut open_card, &fps, &kgt, KeyType::Signing)?;
+        println!("SIGNING_KEY: {:#?}", sign_info);
+
+        let auth_info = get_key_info(&mut open_card, &fps, &kgt, KeyType::Authentication)?;
+        println!("AUTHENTICATION_KEY: {:#?}", auth_info);
+
+        let enc_info = get_key_info(&mut open_card, &fps, &kgt, KeyType::Decryption)?;
+        println!("DECRYPTION_KEY: {:#?}", enc_info);
     }
 
     Ok(())
+}
+
+pub fn get_key_info(
+    card: &mut Card<Transaction>,
+    fps: &KeySet<Fingerprint>,
+    kgt: &KeySet<KeyGenerationTime>,
+    key_type: KeyType,
+) -> Result<KeySlotInfo> {
+    let mut key_info = KeySlotInfo::default();
+    let ki = card.key_information().ok().flatten();
+
+    if let Some(fp) = fps.signature() {
+        key_info.fingerprint = Some(fp.to_hex());
+    }
+
+    key_info.algorithm = Some(card.algorithm_attributes(key_type)?);
+
+    if let Some(uif) = card.user_interaction_flag(key_type)? {
+        key_info.touch_policy = Some(uif.touch_policy());
+        key_info.touch_features = Some(uif.features());
+    }
+
+    if let Ok(PublicKeyMaterial::E(pkm)) = card.public_key_material(key_type) {
+        key_info.public_key_material = Some(pkm.data().to_vec());
+    }
+
+    match key_type {
+        KeyType::Signing => {
+            if let Some(kgt) = kgt.signature() {
+                key_info.creation_time = Some(format!("{}", DateTime::<Utc>::from(kgt)));
+            }
+            key_info.status = ki.map(|ki| ki.sig_status());
+            key_info.signature_count = Some(card.digital_signature_count()?);
+        }
+        KeyType::Authentication => {
+            if let Some(kgt) = kgt.authentication() {
+                key_info.creation_time = Some(format!("{}", DateTime::<Utc>::from(kgt)));
+            }
+            key_info.status = ki.map(|ki| ki.aut_status());
+        }
+        KeyType::Decryption => {
+            if let Some(kgt) = kgt.decryption() {
+                key_info.creation_time = Some(format!("{}", DateTime::<Utc>::from(kgt)));
+            }
+            key_info.status = ki.map(|ki| ki.dec_status());
+        }
+        _ => {}
+    }
+    Ok(key_info)
 }
--- src/setup/openpgp_card.rs
+++ src/setup/openpgp_card.rs
@@ -3,16 +3,43 @@
 */
 
 use crate::{
-    CLI_BLUE,
+    CLI_BLUE, CLI_ORANGE, CLI_PURPLE,
     openpgp_card::{cards, print_cards},
     setup::CommunityDIDKeys,
 };
 use anyhow::Result;
 use console::style;
+use crossterm::{
+    event::{self, Event},
+    terminal,
+};
 
 /// Handles storing secrets on an OpenPGP compatable card
 pub fn setup_hardware_token(keys: &CommunityDIDKeys) -> Result<()> {
     println!();
+
+    println!(
+        "{} {}",
+        style("If you want to use hardware tokens, please ensure they are plugged in now!")
+            .color256(CLI_BLUE),
+        style("(press any key to continue)")
+            .color256(CLI_PURPLE)
+            .blink()
+    );
+    terminal::enable_raw_mode()?;
+    loop {
+        // Read the next event
+        match event::read()? {
+            // If it's a key event and a key press
+            Event::Key(key_event) if key_event.kind == event::KeyEventKind::Press => {
+                break;
+            }
+            _ => {} // Ignore other events (mouse, resize, etc.)
+        }
+    }
+    // Disable raw mode when done
+    terminal::disable_raw_mode()?;
+
     println!(
         "{}",
         style("Looking for openpgp-card compatible tokens...").color256(CLI_BLUE)
@@ -20,7 +47,15 @@ pub fn setup_hardware_token(keys: &CommunityDIDKeys) -> Result<()> {
 
     // Detect cards and show
     let mut cards = cards()?;
-    print_cards(&mut cards)?;
+    if cards.is_empty() {
+        println!(
+            "{}",
+            style("No hardware tokens were found!").color256(CLI_ORANGE)
+        );
+        return Ok(());
+    } else {
+        print_cards(&mut cards)?;
+    }
 
     Ok(())
 }
